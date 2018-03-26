e4c9e6addb4aa24c83c630683424afe5a7439ef7
TMad <madsen.tyler@gmail.com>
2018-03-25 20:42:47 -0600

    Create card enum for set

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,17 @@
 class Card < ApplicationRecord
   has_many :game_cards
+
+  enum set: [:dominion,
+             :intrigue,
+             :seaside,
+             :prosperity,
+             :hinterlands,
+             :dark_ages,
+             :adventures,
+             :empires,
+             :nocturne,
+             :alchemy,
+             :cornucopia,
+             :guilds,
+             :promo]
 end
