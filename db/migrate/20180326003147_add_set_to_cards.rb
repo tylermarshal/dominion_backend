a227a4e988662921c91e2c879f484d768ca425b9
TMad <madsen.tyler@gmail.com>
2018-03-25 20:43:42 -0600

    Add set column to cards

---
+++ db/migrate/20180326003147_add_set_to_cards.rb
@@ -0,0 +1,5 @@
+class AddSetToCards < ActiveRecord::Migration[5.1]
+  def change
+    add_column :cards, :set, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325233459) do
+ActiveRecord::Schema.define(version: 20180326004527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 20180325233459) do
   create_table "cards", force: :cascade do |t|
     t.string "name"
     t.integer "cost"
-    t.string "type"
+    t.string "attack"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "set"
   end
 
   create_table "competitors", force: :cascade do |t|
