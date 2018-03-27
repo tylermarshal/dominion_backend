7489098f966414ed511ac0515e52de1e55357383
maxscores <maxstackhouse@gmail.com>
2018-03-27 12:37:43 -0600

    Add self.new_game  to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,24 @@ class Game < ApplicationRecord
   has_many :decks
   has_many :competitors
   has_many :players, through: :competitors
+
+  after_save :create_decks
+
+  def self.new_game(players)
+    new_game = Game.new
+    if Player.exists?(id: players)
+      players.each do |player_id|
+        player = Player.find(player_id)
+        new_game.players << player
+      end
+      new_game.game_cards << GameCard.new_game
+    end
+    new_game
+  end
+
+  def create_decks
+    competitors.each do |competitor|
+      decks << Deck.new_game(competitor.id)
+    end
+  end
 end
