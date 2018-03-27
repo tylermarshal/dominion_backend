988fe30f51635b0267192cd0ccbd816e27d879be
maxscores <maxstackhouse@gmail.com>
2018-03-27 12:37:51 -0600

    Add self.new_game to deck

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,17 @@
 class Deck < ApplicationRecord
-  belongs_to :player
+  belongs_to :competitor
   belongs_to :game
+
+  def self.new_game(competitor_id)
+    new(competitor_id: competitor_id,
+        draw: starter_deck.shuffle,
+        discard: [])
+  end
+
+  def self.starter_deck
+    deck = []
+    7.times {deck.push(32)}
+    3.times {deck.push(36)}
+    deck
+  end
 end
