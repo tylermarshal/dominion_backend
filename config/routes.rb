7a64580454ecf57313e141af74c0265b2a1a8e7d
maxscores <maxstackhouse@gmail.com>
2018-03-31 17:57:15 -0600

    Add Post Turn to Game Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      post "/games", to: "games#create", as: "new_game"
+      post '/games', to: 'games#create'
+      post '/games/:id/turns', to: 'turns#create'
     end
   end
 end
