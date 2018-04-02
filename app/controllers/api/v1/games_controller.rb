8f576db30204896dd811c34498e779dba0c4eec2
maxscores <maxstackhouse@gmail.com>
2018-04-02 07:55:41 -0600

    Add Get Game Method to Controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::GamesController < ApplicationController
+  def show
+    game = Game.find(params[:id])
+    if game
+      render json: game
+    end
+  end
+
   def create
     new_game = Game.new_game(params["competitors"])
     if new_game.save!
