ef050e3ee0f7c9dfa5208276d2367e06c95e3cc7
maxscores <maxstackhouse@gmail.com>
2018-04-10 13:56:24 -0600

    Increment next player on record_turn

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -9,6 +9,7 @@ class Turn < ApplicationRecord
 		game.update_attacks(params["attack_stack"])
     game.update_game_card_quantities(params['supply'])
     game.update_trash(params['trash'])
+		game.set_current_player
     turn = Turn.new(
       game: game,
       competitor_id: game.competitors.find_by(player_id: game.current_player).id,
