1d6c520584a6ed1baa6959bd209df2997fd73629
maxscores <maxstackhouse@gmail.com>
2018-04-14 12:49:23 -0600

    Add token to player

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180414173519) do
+ActiveRecord::Schema.define(version: 20180414183405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20180414173519) do
     t.string "username"
     t.string "password_digest"
     t.string "phone_number"
+    t.string "token"
   end
 
   create_table "turns", force: :cascade do |t|
