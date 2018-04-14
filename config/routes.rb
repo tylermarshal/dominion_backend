74a73ac2888ed35f406aa3c31ee297b2c5385e02
maxscores <maxstackhouse@gmail.com>
2018-04-14 13:40:15 -0600

    Add player search endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
 			post '/players', to: 'players#create'
 			get '/player', to: 'players#show'
+			get '/players', to: 'players#index'
       post '/games', to: 'games#create'
       get '/games/:id', to: 'games#show'
       post '/games/:id/turns', to: 'turns#create'
