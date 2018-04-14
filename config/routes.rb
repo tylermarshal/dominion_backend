77538805791e36b6e93b471e33f4dbb642b33fec
maxscores <maxstackhouse@gmail.com>
2018-04-14 12:19:54 -0600

    Add create players endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+			post '/players', to: 'players#create'
       post '/games', to: 'games#create'
       get '/games/:id', to: 'games#show'
       post '/games/:id/turns', to: 'turns#create'
