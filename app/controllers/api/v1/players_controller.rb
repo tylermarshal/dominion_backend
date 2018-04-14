e941ced58fc38ebfb5be38c9c4399d4f448e0c98
maxscores <maxstackhouse@gmail.com>
2018-04-14 13:40:31 -0600

    Add player search controller endpoint

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,11 +1,7 @@
 class Api::V1::PlayersController < ApplicationController
-	def create
-		new_player = Player.new(user_params)
-		if new_player.save
-			render json: new_player
-		else
-			render json: {message: 'Username and Phone Number must be unique'}, status: 400
-		end
+	def index
+		players = Player.where('username LIKE ?', "%#{search_params[:user_query]}%")
+		render json: players, serializer: PlayerSearchSerializer
 	end
 
 	def show
@@ -17,8 +13,21 @@ class Api::V1::PlayersController < ApplicationController
 		end
 	end
 
+	def create
+		new_player = Player.new(user_params)
+		if new_player.save
+			render json: new_player
+		else
+			render json: {message: 'Username and Phone Number must be unique'}, status: 400
+		end
+	end
+
 	private
 		def user_params
 			params.permit(:username, :password, :phone_number)
+		end
+
+		def search_params
+			params.permit(:user_query)
 		end
 end
