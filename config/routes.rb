2dd40f419785dfe6910231c6db84f1af3afdbc98
maxscores <maxstackhouse@gmail.com>
2018-04-14 14:01:29 -0600

    Change endpoint names

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-			post '/players', to: 'players#create'
-			get '/player', to: 'players#show'
+			post '/friends', to: 'friends#create'
+			post '/signup', to: 'players#create'
+			get '/login', to: 'players#show'
 			get '/players', to: 'players#index'
       post '/games', to: 'games#create'
       get '/games/:id', to: 'games#show'
