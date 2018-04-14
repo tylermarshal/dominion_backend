9b24db28dd7bdf2dbda8110a59b8cc0e05cd6f34
maxscores <maxstackhouse@gmail.com>
2018-04-14 14:01:41 -0600

    Add belongs_to / has_many friends

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,7 +3,8 @@ class Player < ApplicationRecord
 	validates_uniqueness_of :username, :phone_number
   has_many :competitors
   has_many :games, through: :competitors
-
+	has_many :friends
+	
 	before_save :generate_token
 
 	def generate_token
