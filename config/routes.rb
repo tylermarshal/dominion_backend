4d9e111635d626d66f9ba768af6d325cc78323eb
maxscores <maxstackhouse@gmail.com>
2018-04-14 14:53:29 -0600

    Add Controller Method to delete friend

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 			post '/friends', to: 'friends#create'
+			delete '/friends', to: 'friends#delete'
 			post '/signup', to: 'players#create'
 			get '/login', to: 'players#show'
 			get '/players', to: 'players#index'
