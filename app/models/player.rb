70d62810e43b1009214ea0531db40688b4fa1366
maxscores <maxstackhouse@gmail.com>
2018-04-14 12:08:19 -0600

    Add Migration for Players username, password, phone_number

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
+	has_secure_password
+	validates_uniqueness_of :username, :phone_number
   has_many :competitors
   has_many :games, through: :competitors
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409185321) do
+ActiveRecord::Schema.define(version: 20180414173519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,9 @@ ActiveRecord::Schema.define(version: 20180409185321) do
   create_table "players", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "password_digest"
+    t.string "phone_number"
   end
 
   create_table "turns", force: :cascade do |t|
