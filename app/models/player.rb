89785a8f970645d38e07b7c4b34dc93a9fa2f62b
maxscores <maxstackhouse@gmail.com>
2018-04-14 12:50:53 -0600

    Generate Token before player save

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,10 @@ class Player < ApplicationRecord
 	validates_uniqueness_of :username, :phone_number
   has_many :competitors
   has_many :games, through: :competitors
+
+	before_save :generate_token
+
+	def generate_token
+		self.token = SecureRandom.base64.tr('+/=', 'Qrt')
+	end
 end
