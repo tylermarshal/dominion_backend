a23e9b886066525f90b0b4312ee937d739ec3a2d
maxscores <maxstackhouse@gmail.com>
2018-04-16 13:41:42 -0600

    Add friends to player serializer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,9 +1,15 @@
 class PlayerSerializer < ActiveModel::Serializer
-	attributes :id, :username, :token, :games
+	attributes :id, :username, :token, :games, :friends
 
 	def games
 		object.games.map do |game|
 			{id: game.id, players: game.players.pluck(:username)}
+		end
+	end
+
+	def friends
+		object.friends.map do |friend|
+			{id: friend.friend.id, username: friend.friend.username}
 		end
 	end
 end
