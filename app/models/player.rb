3f4a46dacb9eb8fcf8ecbfd08e804f921c2172ac
maxscores <maxstackhouse@gmail.com>
2018-04-16 17:38:38 -0600

    Remove Before Save Make token

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,10 +4,4 @@ class Player < ApplicationRecord
   has_many :competitors
   has_many :games, through: :competitors
 	has_many :friends
-	
-	before_save :generate_token
-
-	def generate_token
-		self.token = SecureRandom.base64.tr('+/=', 'Qrt')
-	end
 end
