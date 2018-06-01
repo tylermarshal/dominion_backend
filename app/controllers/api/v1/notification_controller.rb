be85afa844ee366df11ecd007667839c019f0970
maxscores <maxstackhouse@gmail.com>
2018-05-31 18:11:42 -0600

    Filter for token

--- app/controllers/api/v1/notification_controller.rb
+++ app/controllers/api/v1/notification_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::NotificationController < ApplicationController
 	def create
 		game = Game.find(params[:id])
-		current_player = Game.find(game.current_player)
-		game.send_turn_notification(current_player)
+		current_player = Player.find(game.current_player)
+		if current_player.token
+			game.send_turn_notification(current_player)
+		end
+		render status: 200
 	end
 end
