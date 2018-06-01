e8b7fb00bc33ceed88b1f5953c15f215fcfd2e02
maxscores <maxstackhouse@gmail.com>
2018-05-31 18:05:02 -0600

    send current player

--- app/controllers/api/v1/notification_controller.rb
+++ app/controllers/api/v1/notification_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::NotificationController < ApplicationController
 	def create
 		game = Game.find(params[:id])
-		game.send_turn_notification(game.current_player)
+		current_player = Game.find(game.current_player)
+		game.send_turn_notification(current_player)
 	end
 end
